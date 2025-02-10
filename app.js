add5ce0d78aab3749fc0b1536d2bf85faa7ecbae
adtyrai <abhi38020@gmail.com>
2025-02-10 11:03:55 +0530

    branch1-commit6

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const API_KEY = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY';
 
 app.get('/', (req, res) => {
   res.send('Default route');
